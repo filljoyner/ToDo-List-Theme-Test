1fbe5acf6eca2bf1c12389e67795d41fbf1ba617
Philip Joyner <filljoyner@gmail.com>
2019-10-13 18:56:26 -0500

    loading wp force login package and creatine type/action routing

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 /** Require WPM */
+
 require_once __DIR__ . '/_wpm/wpm.php';
 
 /** Import Config */
@@ -8,4 +9,18 @@ require_once __DIR__ . '/config/boot.php';
 
 
 /** Load Packages */
+package('wp-force-login');
 package('bootstrap4');
+
+
+/** Type and Action Routing */
+if(!empty($_GET['type']) and !empty($_GET['action']) and get_current_user_id() and !is_admin()) {
+    $type = $_GET['type'];
+    $action = $_GET['action'];
+
+    if($type == 'todo_list') {
+        todo_list_requests($_POST)->action($action);
+    }
+
+    redirect(home_url());
+}
